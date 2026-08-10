b974efc5abfd3441c930b67bdf087ce590e2c84c
Mubin Ali <mobinali2150@gmail.com>
2026-08-10 11:12:14 -0700

    fix vercel static files

--- db.js
+++ db.js
@@ -1,3 +1,5 @@
+ require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
@@ -5,11 +7,10 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306
+    port: Number(process.env.DB_PORT || 3306)
 });
 
 connection.connect((err) => {
-
     if (err) {
         console.error("Database connection error:", err);
         return;
